c55c76fadd413a745baa192feaeab23905bd6b2f
abhishek1709-vesit <abhishekgore1706@gmail.com>
2025-12-02 15:24:27 +0530

    Dummy changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -60,18 +60,17 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
   // Update timestamp
   this.updatedAt = Date.now();
   
   // Hash password if modified
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Method to compare passwords
